9559e1903a17c1532abf1475f89a0f0d13a07451
FEPOFIGHT <94674437+fighter231456@users.noreply.github.com>
2021-12-28 13:06:06 +0700

    commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -68,6 +68,7 @@
                   </span><br> <span>Good luck and have fun
                     </span>
                     </p>
+                    <hr id="divider03">
                     <!-- <hr id="divider03">
                     </div>
                     </div>
@@ -103,33 +104,33 @@
 </template>
 <script>
 // Set the date we're counting down to
-var countDownDate = new Date("Dec 31 2021 24:00:00").getTime();
+// var countDownDate = new Date("Dec 31 2021 24:00:00").getTime();
 
-// Update the count down every 1 second
-var x = setInterval(function() {
+// // Update the count down every 1 second
+// var x = setInterval(function() {
 
-  // Get today's date and time
-  var now = new Date().getTime();
+//   // Get today's date and time
+//   var now = new Date().getTime();
 
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
+//   // Find the distance between now and the count down date
+//   var distance = countDownDate - now;
 
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+//   // Time calculations for days, hours, minutes and seconds
+//   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+//   // Display the result in the element with id="demo"
+//   document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+//   + minutes + "m " + seconds + "s ";
 
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
-  }
-}, 1000);
+//   // If the count down is finished, write some text
+//   if (distance < 0) {
+//     clearInterval(x);
+//     document.getElementById("demo").innerHTML = "EXPIRED";
+//   }
+// }, 1000);
 </script>
 <script>
 export default {
@@ -211,4 +212,11 @@ p {
   font-size: 60px;
   margin-top: 0px;
 }
+h4{
+  color: #a1df2e;
+}
+a[data-v-469af010] {
+    color: #f5f5ff;
+}
+
 </style>
